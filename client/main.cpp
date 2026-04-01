5c474bf310b5db70a251aedbb04dd09f515d1e17
Ruben <ruben.barros@student.supsi.ch>
2026-04-01 12:55:17 +0200

    added .oct files to gitignore

--- client/main.cpp
+++ client/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
 	glm::mat4 lightPos = glm::translate(glm::mat4(1.0f), glm::vec3(5.0f, 5.0f, 5.0f));
 	eng.setNodeTransform(lightId, lightPos);
 
-	eng.addNodeFromFile(modelPath+"/piramid.glb");
+	eng.addNodeFromFile(modelPath+"cube.glb");
 
 	eng.bindSceneEvent(3, moveCameraEvent);
 	eng.bindSceneEvent('w', 3, cameraUpEvent);
